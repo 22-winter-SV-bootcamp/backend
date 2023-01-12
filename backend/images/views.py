b7106915f9031acb1ee8ae77b2edc369277d0139
Ellie <doris0707@naver.com>
2023-01-11 22:37:13 -0800

    chore: celery basic setting

--- backend/images/views.py
+++ backend/images/views.py
@@ -1,2 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
+
+
+class Images(APIView):
+    def get(self, request):
+        return 
+
+    def post(self, request):
+        img = request.FILES.get('file')
+        
+        return
--- backend/styles/urls 2.py
+++
@@ -1,5 +0,0 @@
-from images import views
-from django.urls import include, path
-
-urlpatterns = [
-]
