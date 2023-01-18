b2b0fca8bd846bf2663f35cdf943588e23c6ad28
Jmmk <jmk525@gmail.com>
2023-01-18 15:01:20 +0900

    chore: 주석 제거

--- backend/images/views.py
+++ backend/images/views.py
@@ -10,17 +10,14 @@ from .serializers import imageSerializer
 @api_view(['GET'])
 def recentImage(request):
     if request.method == 'GET':
-
         pages = request.GET.get('page')
         recent_list = image.objects.all().order_by('-created_at')   #최근 이미지 별로 정렬
         link_list = recent_list.values('link')  # 모델에서 링크만 추출
         paginator = Paginator(link_list,5)  # 링크 5개씩 페이지네이션
-        
         try:
             list = paginator.page(pages) # 보여줄 페이지는 쿼리스트링 값
             res = list.object_list
             serializer = imageSerializer(res, many=True)
-            # if serializer.is_valid():
             return JsonResponse(serializer.data,status=status.HTTP_200_OK, safe=False)
         # 예외 처리
         except PageNotAnInteger:    
@@ -31,3 +28,4 @@ def recentImage(request):
         
         
         
+
