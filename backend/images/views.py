957f4835e174916a675c676626aa284fa8eeb1dd
Jmmk <jmk525@gmail.com>
2023-01-19 23:11:30 +0900

    feat: timeout 이용해서 task 존재 여부 판단기능 추가

--- backend/images/views.py
+++ backend/images/views.py
@@ -12,6 +12,7 @@ from .tasks import ai_task
 from .serializers import imageSerializer
 from images.result import task_result
 import json
+from celery.exceptions import TimeoutError
 
 
 class Images(APIView):
@@ -44,48 +45,53 @@ def get_task(request):
     if request.method == 'GET':
         try:
             task_id = request.GET.get('task_id')
+            AsyncResult(task_id).get(timeout=5)         # 5초동안 못찾으면 없는 task
             task = AsyncResult(task_id)
             
-            with open('images/sort/bottom.txt') as f:
-                bot = f.read().splitlines()
-                bot_def = bot[bot.index('slacks')]
-            with open('images/sort/top.txt') as g:
-                top = g.read().splitlines()
-                top_def = top[top.index('t-shirts')]  
+            if task.ready():                          
+                with open('images/sort/bottom.txt') as f:
+                    bot = f.read().splitlines()
+                    bot_def = bot[bot.index('slacks')]
+                with open('images/sort/top.txt') as g:
+                    top = g.read().splitlines()
+                    top_def = top[top.index('t-shirts')]  
             
-            to_list = list(task.result.values())       # dict -> list 
-            to_str = ' '.join(to_list)                 # list -> string
-            lists = to_str.split()                     # string -> list
-            dict = {string:i for i, string in enumerate(lists)}     # confidence별로 dict의 value값 지정 (낮을수록 높은 confidence)
-            bot_list = []
-            top_list = []    
+                to_list = list(task.result.values())       # dict -> list 
+                to_str = ' '.join(to_list)                 # list -> string
+                lists = to_str.split()                     # string -> list
+                dict = {string:i for i, string in enumerate(lists)}     # confidence별로 dict의 value값 지정 (낮을수록 높은 confidence)
+                bot_list = []
+                top_list = []    
 
-            try:
-                for key in dict:
-                    if key in bot:
-                        bot_list.append(key)
-                    elif key in top:
-                        top_list.append(key)
+                try:
+                    for key in dict:
+                        if key in bot:
+                            bot_list.append(key)
+                        elif key in top:
+                            top_list.append(key)
                 
-                large_top = top_list[0]             # 항상 0번째 인덱스값이 가장 높은 컨피던스를 가짐
-                large_bot = bot_list[0]             # ""
+                    large_top = top_list[0]             # 항상 0번째 인덱스값이 가장 높은 컨피던스를 가짐
+                    large_bot = bot_list[0]             # ""
                 
-            except:                                 # ai가 인식못했을때
-                if not top_list and bot_list: 
-                    large_top = top_def
-                    large_bot = bot_list[0]
+                except:                                 # ai가 인식못했을때
+                    if not top_list and bot_list: 
+                        large_top = top_def
+                        large_bot = bot_list[0]
                     
-                elif top_list and not bot_list:
-                    large_top = top_list[0]
-                    large_bot = bot_def
-                else:
-                    large_top = top_def
-                    large_bot = bot_def
+                    elif top_list and not bot_list:
+                        large_top = top_list[0]
+                        large_bot = bot_def
+                    else:
+                        large_top = top_def
+                        large_bot = bot_def
                     
-            finally:
-                res = task_result('done',{'top' : large_top, 'bottom' : large_bot})
-                return JsonResponse(res,status=200)
-        except:                     # task가 안끝났을때
-            res = task_result('Not yet')    
+                finally:
+                    res = task_result('done',{'top' : large_top, 'bottom' : large_bot})
+                    return JsonResponse(res,status=200)
+            else:                           # task가 안끝났을때
+                res = task_result('Not yet')    
+                return JsonResponse(res,status=404)
+        except TimeoutError:                             # 없는 task_id일때
+            res = task_result('task does not exist')    
             return JsonResponse(res,status=404)
             
