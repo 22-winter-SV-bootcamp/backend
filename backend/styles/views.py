7aa8b6bac744b34759be274700b1f3d8507bef15
Ellie <doris0707@naver.com>
2023-01-26 20:01:17 +0900

    chore: ai mod

--- backend/styles/views.py
+++ backend/styles/views.py
@@ -30,18 +30,18 @@ class ShowStyleView(APIView):
                 status.HTTP_404_NOT_FOUND: "Error"}
                 )
     def post(self,request): 
-        try:
-            data = request.FILES.get('file')
-            url = get_image_url(data)
-            
-            image.objects.create(link=url)
-            style_info = style(gender = request.POST['gender'],top=request.POST['top'],top_color=request.POST['top_color'],bottom=request.POST['bottom'],bottom_color=request.POST['bottom_color']
-            ,image_id=image.objects.get(link=url))
-            
-    
-            style_info.save()
-            return JsonResponse({"link": url},status=200)
+        # try:
+        data = request.FILES.get('file')
+        url = get_image_url(data)
+        
+        image.objects.create(link=url)
+        style_info = style(gender = request.POST['gender'],top=request.POST['top'],top_color=request.POST['top_color'],bottom=request.POST['bottom'],bottom_color=request.POST['bottom_color']
+        ,image_id=image.objects.get(link=url))
+        
 
-        except:       
-            return JsonResponse({"Error":"problem with received form-data"},status=status.HTTP_404_NOT_FOUND)
+        style_info.save()
+        return JsonResponse({"link": url},status=200)
+
+        # except:       
+        #     return JsonResponse({"Error":"problem with received form-data"},status=status.HTTP_404_NOT_FOUND)
         
