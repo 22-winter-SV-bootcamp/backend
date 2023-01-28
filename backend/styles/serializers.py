7fdade3c30e3b09fcd8633ec4dce25e23012ca16
Jmmk <jmk525@gmail.com>
2023-01-28 20:52:04 +0900

    chore: styles serializer 삭제

--- backend/styles/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import style
-
-class styleSerializer(ModelSerializer):
-    class Meta:
-        model = style
-        fields = ('__all__')
-       
-        
