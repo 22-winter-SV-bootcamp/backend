19c9c74c4c9a03bad48bd3a0fb8d81d24cf5126b
Ellie <doris0707@naver.com>
2023-01-30 13:14:45 +0900

    chore: ai server error

--- ai/ai/settings.py
+++ ai/ai/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     # monitoring
-    'django_prometheus'
+    'django_prometheus',
     'images',
     'corsheaders',
 ]
